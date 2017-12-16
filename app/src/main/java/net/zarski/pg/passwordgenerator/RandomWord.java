0a3aff64e86e710ef19e3b4c054a572c41c5f275
Wojciech Zarski <wojciech.zarski@gmail.com>
2017-12-16 23:30:10 +0100

    Small refactoring and code cleanup.

--- app/src/main/java/net/zarski/pg/passwordgenerator/RandomWord.java
+++ app/src/main/java/net/zarski/pg/passwordgenerator/RandomWord.java
@@ -1,55 +1,76 @@
 package net.zarski.pg.passwordgenerator;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.RandomAccessFile;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.Random;
+import java.io.*;
+import java.util.*;
 
 /**
  * Created by lb_lb on 27.08.16.
  */
-public class RandomWord implements Iterable<String> {
-    private ArrayList<String> words = new ArrayList();
-    Random random = new Random(System.nanoTime());
-    public RandomWord(InputStream stream) throws IOException {
-        InputStreamReader r = new InputStreamReader(stream);
-        BufferedReader br = new BufferedReader(r);
-        String line;
-        while((line = br.readLine()) != null){
-            words.add(line);
-        }
+public class RandomWord implements Iterable<String>
+{
+    private final List<String> words;
 
+	Random random = new Random(System.nanoTime());
+
+	public RandomWord(InputStream stream) throws IOException
+	{
+		words  = new ArrayList<>();
+
+		loadAllWordsToMemmory(stream, new OnWord() {
+				@Override
+				public void onWord(String word)
+				{
+					words.add(word);
+				}
+			});
     }
 
-    public String getWord(){
+	private void loadAllWordsToMemmory(InputStream stream, OnWord onWord) throws IOException
+	{
+		InputStreamReader r = new InputStreamReader(stream);
+        BufferedReader br = new BufferedReader(r);
+        String line;
+        while ((line = br.readLine()) != null)
+		{
+			onWord.onWord(line);
+        }
+	}
+
+	private static interface OnWord
+	{
+		void onWord(String word);
+	}
+
+    public String getWord()
+	{
         return randomWord();
     }
 
-    private String randomWord(){
+    private String randomWord()
+	{
         int index = random.nextInt(words.size());
         return words.get(index);
     }
 
     @Override
-    public Iterator<String> iterator() {
+    public Iterator<String> iterator()
+	{
         return new Iterator<String>() {
             @Override
-            public boolean hasNext() {
+            public boolean hasNext()
+			{
                 return true;
             }
 
             @Override
-            public String next() {
+            public String next()
+			{
                 return getWord();
             }
 
             @Override
-            public void remove() {
+            public void remove()
+			{
                 //do nothing
             }
         };
